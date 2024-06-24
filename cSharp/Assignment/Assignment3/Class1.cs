950085086d97a6c7b4fc4f6e17b5f47b9d1fb95b
vijendra-infinite <vijendratr@infinite.com>
2024-06-24 23:49:57 +0530

    Add files via upload

---
+++ cSharp/Assignment/Assignment3/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    public class Students
+    {
+        public int RollNo;
+        public string Name;
+        public string Class;
+        public int Semester;
+        public string Branch;
+        public int[] marks = new int[5];
+
+        public Students(int rollNo, string name, string Classs, int semester, string branch)
+        {
+            RollNo = rollNo;
+            Name = name;
+            Class = Classs;
+            Semester = semester;
+            Branch = branch;
+        }
+
+        public void GetMarks()
+        {
+            Console.WriteLine($"Enter marks for Roll No {RollNo}, {Name}");
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.Write($"Enter marks for Subject {i + 1}: ");
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public double DisplayResult()
+        {
+            double sum = 0;
+            foreach (int mark in marks)
+            {
+                sum += mark;
+            }
+            return sum / marks.Length;
+        }
+
+
+        public void DisplayData()
+        {
+            Console.WriteLine($"Student_Roll_Number = {RollNo}");
+            Console.WriteLine($"Student_Name = {Name}");
+            Console.WriteLine($"Student_Class = {Class}");
+            Console.WriteLine($"Student_Semester = {Semester}");
+            Console.WriteLine($"Student_Branch = {Branch}");
+
+            Console.WriteLine("........Displaying student average marks with result..........");
+            Console.WriteLine($"Student_Marks");
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine($"Subject {i + 1} marks : {marks[i]}");
+            }
+
+            double averageMarks = DisplayResult();
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"Average Marks: {averageMarks}");
+            Console.WriteLine("----------------------");
+            if (marks.Any(m => m < 35))
+            {
+                Console.WriteLine("RESULT: Failed (Marks of one or more subjects are less than 35)");
+            }
+            else if (averageMarks < 50)
+            {
+                Console.WriteLine("RESULT: Failed (Average marks are less than 50)");
+            }
+            else
+            {
+                Console.WriteLine("RESULT: Passed");
+            }
+
+
+        }
+
+
+    }
+
+
+    class StudentDetails
+    {
+        static void Main()
+        {
+            Students s = new Students(123, "AADI", "FIFTH", 4, "ECE");
+            Console.WriteLine(".......Please Enter Student Marks.........");
+            s.GetMarks();
+            Console.WriteLine("........Student Results with Info.........");
+            s.DisplayData();
+            Console.Read();
+        }
+    }
+}
---
+++ cSharp/Assignment/Assignment3/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    public class Accounts
+    {
+        public int AccountNo;
+        public string CustomerName;
+        public string AccountType;
+        public char TransactionType;
+        public double Amount;
+        public double Balance;
+
+        public Accounts(int accNum, string cName, string accType, char transacType, double amount, double bal)
+        {
+            AccountNo = accNum;
+            CustomerName = cName;
+            AccountType = accType;
+            TransactionType = transacType;
+            Amount = amount;
+            Balance = bal;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine($"Account Number: {AccountNo}");
+            Console.WriteLine($"Customer Name: {CustomerName}");
+            Console.WriteLine($"Account Type(Savings/Current/Salary): {AccountType}");
+            Console.WriteLine($"Transaction Type(D-Deposit;W-Withdrawl): {TransactionType}");
+            Console.WriteLine($"Amount: {Amount}");
+            Console.WriteLine($"Balance: {Balance}");
+        }
+
+        public void Credit(int amount)
+        {
+            Balance += amount;
+        }
+
+        public void Debit(int amount)
+        {
+            if (Balance > amount)
+                Balance -= amount;
+            else
+                Console.WriteLine("Insufficent Balance to withdraw");
+        }
+
+
+    }
+    class AccountDetails
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Enter Account Number:");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Customer Name:");
+            string customerName = Console.ReadLine();
+
+            Console.WriteLine("Enter Account Type:");
+            string accountType = Console.ReadLine();
+
+            Console.WriteLine("Enter Transaction Type (D for deposit, W for withdrawal):");
+            char transactionType = char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            Console.WriteLine("Enter Amount:");
+            double amount = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Accounts ac = new Accounts(accountNumber, customerName, accountType, transactionType, amount, 10000);
+
+            //Accounts ac = new Accounts(123456, "John Doe", "Savings", 'D', 1000.00, 5000.00);
+
+            if (ac.TransactionType == 'D')
+                ac.Credit((int)ac.Amount);
+            else if (ac.TransactionType == 'W')
+                ac.Debit((int)ac.Amount);
+            else
+                Console.WriteLine("incorrect method");
+
+            ac.ShowData();
+            Console.Read();
+
+        }
+    }
+}
