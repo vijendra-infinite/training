b085a125942dd55cc881c5d912888ae618f5dee9
vijendra-infinite <vijendratr@infinite.com>
2024-08-14 11:52:09 +0530

    Add files via upload

---
+++ MVC T/Assessment/CC9_MVC_Solution2/Repository/IMoviesRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC9_MVC_SOLUTION2.Repository
+{
+    public interface IMovieRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(object Id);  //a particular movie
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(Object Id);
+        void Save();
+    }
+
+}
